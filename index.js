b9c139ab53b73db469d8d50d30fe196996a353cd
Deepti Shanbhag <shanbhxg@gmail.com>
2024-03-22 15:23:44 +0530

    Dist.

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
 const url = require('url');
 const path = require('path');
 
-
+let mainWindow;
 
 function createMainWindow() {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         title: 'My Tool App',
         height: 1000,
         width: 1000,
         nodeIntegration: true,
         contextIsolation: false,
         webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false
+            nodeIntegration: true,
+            contextIsolation: false
         }
-
     });
 
     mainWindow.loadFile('./login.html');
 }
-  
 
-app.whenReady().then(() => {
+function checkForUpdates() {
+    autoUpdater.checkForUpdatesAndNotify();
+}
+
+app.on('ready', () => {
     createMainWindow();
-
-   
+    checkForUpdates();
 });
-
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'win32') {
         app.quit();
     }
-})
+});
+
+autoUpdater.on('update-available', () => {
+    console.log('Update available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+    console.log('Update downloaded');
+    autoUpdater.quitAndInstall();
+});
