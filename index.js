63c18e963b6113af7cb34c3d1a628c8a46b46257
shanbhxg <shanbhxg@gmail.com>
2024-03-26 13:47:25 +0530

    modified:   login.html

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 let win;
 
 function sendStatusToWindow(text) {
-    log.info(text);
+    console.log(text);
     win.webContents.send('message', text);
   }
 
@@ -34,24 +34,6 @@ function sendStatusToWindow(text) {
 autoUpdater.on('checking-for-update', () => {
     sendStatusToWindow('Checking for update...');
   })
-  autoUpdater.on('update-available', (info) => {
-    sendStatusToWindow('Update available.');
-  })
-  autoUpdater.on('update-not-available', (info) => {
-    sendStatusToWindow('Update not available.');
-  })
-  autoUpdater.on('error', (err) => {
-    sendStatusToWindow('Error in auto-updater. ' + err);
-  })
-  autoUpdater.on('download-progress', (progressObj) => {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-    sendStatusToWindow(log_message);
-  })
-  autoUpdater.on('update-downloaded', (info) => {
-    sendStatusToWindow('Update downloaded');
-  });
 
 app.whenReady().then(() => {
     
