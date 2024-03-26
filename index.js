dd97bed64d37a6a2835cc9e174e06f9d585f6e31
shanbhxg <shanbhxg@gmail.com>
2024-03-26 13:17:35 +0530

    Revert "	modified:   package.json"
    This reverts commit f7fa48287f13f9d9f480aabf85b73a20a0252914.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-
-const { app, BrowserWindow, dialog } = require('electron');
-const { autoUpdater } = require('electron-updater');
+const { app, BrowserWindow } = require('electron');
+const url = require('url');
 const path = require('path');
+
+
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
@@ -11,81 +12,30 @@ function createMainWindow() {
         nodeIntegration: true,
         contextIsolation: false,
         webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
+        nodeIntegration: true,
+        contextIsolation: false
+        }
+
+    });
+
+    mainWindow.loadFile('./login.html');
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow();
         }
     });
 
-    mainWindow.loadFile(path.join(__dirname, 'login.html'));
-}
-
-//      Hazel Releases
-/*
-app.on('ready', () => {
-    createMainWindow();
-
-    /*autoUpdater.setFeedURL({
-        provider: 'github',
-        owner: '20nikhil02',
-        repo: 'Electron-Calci',
-        private: false 
-    });*
-
-    
-    setInterval(() => {
-        autoUpdater.checkForUpdates();
-    }, 60000);
-
-    
-    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-        const dialogOpts = {
-            type: 'info',
-            buttons: ['Restart', 'Later'],
-            title: 'Application Update',
-            message: process.platform === 'win32' ? releaseNotes : releaseName,
-            detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-        };
-
-        dialog.showMessageBox(dialogOpts).then((returnValue) => {
-            if (returnValue.response === 0) autoUpdater.quitAndInstall();
-        });
-    });
-
-    
-    autoUpdater.on('error', (message) => {
-        console.error('There was a problem updating the application');
-        console.error(message);
-    });
-});*/
-
-
-
-//          GitHub Release
-app.on('ready', () => {
-    createMainWindow();
-
-    autoUpdater.setFeedURL({
-        provider: 'github',
-        owner: '20nikhil02',
-        repo: 'Electron-Calci',
-        private: true 
-    });
-
-    autoUpdater.checkForUpdatesAndNotify();
+    autoUpdater.checkForUpdates();
 });
 
-autoUpdater.on("update-available",()=>{
-    dialog.showMessageBox("update-available");
-})
 
-autoUpdater.on("checking-for-update",()=>{
-    dialog.showMessageBox("update-available");
-})
-
-autoUpdater.on("download-progress",()=>{
-    dialog.showMessageBox("update-available");
-})
-
-autoUpdater.on("update-downloaded",()=>{
-    dialog.showMessageBox("update-downloaded");
+app.on('window-all-closed', () => {
+    if (process.platform !== 'win32') {
+        app.quit();
+    }
 })
